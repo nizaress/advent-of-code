3d1903793d58ee5302dc73a3beaacffa5ec6dbd0
Nizaress <gfdnortes@gmail.com>
2025-12-24 02:33:42 +0100

    2025: Day 11

--- 2025/Day-11/part1.cpp
+++ 2025/Day-11/part1.cpp
@@ -3,7 +3,10 @@
 #include <vector>
 #include <string>
 #include <unordered_map>
-#include <limits>
+
+
+// Memory for the search
+std::unordered_map<unsigned int, long long> memory_paths;
 
 
 void get_data(std::vector<std::vector<unsigned int>> &connections, std::string file_name){
@@ -12,6 +15,8 @@ void get_data(std::vector<std::vector<unsigned int>> &connections, std::string f
 
     unsigned long next_rack_id{ 2 };
     std::unordered_map<std::string, unsigned int> rack_ids;
+    rack_ids.emplace("you", 0);
+    rack_ids.emplace("out", 1);
 
     if(file.is_open()){
     	while(getline(file,line)){
@@ -19,78 +24,85 @@ void get_data(std::vector<std::vector<unsigned int>> &connections, std::string f
             unsigned int last_pos{};
 
             for(unsigned int i{}; i<line.size(); ++i){
-                std::cout << line[i] << std::flush;
                 if(last_pos > i) continue;
 
                 if(line[i]==':'){
-                    if(line.substr(0,i) == "you") current_id = 0;
-                    else current_id = next_rack_id++;
-                    rack_ids.emplace(line.substr(0,i), current_id);
+                    if(rack_ids.find(line.substr(0,i)) != rack_ids.end())
+                        current_id = rack_ids[line.substr(0,i)];
+                    else{
+                        rack_ids.emplace(line.substr(0,i), next_rack_id);
+                        current_id = next_rack_id++;
+                        connections.push_back(std::vector<unsigned int>());
+                    }
                     last_pos = i + 2;
                 }
 
                 if(i==line.size()-1){
-                    if(rack_ids.find(line.substr(last_pos, (i-last_pos))) != rack_ids.end()){
-                        connections[current_id].push_back(rack_ids[line.substr(last_pos, (i-last_pos))]);
+                    if(rack_ids.find(line.substr(last_pos, (i+1-last_pos))) != rack_ids.end()){
+                        connections[current_id].push_back(rack_ids[line.substr(last_pos, (i+1-last_pos))]);
                     }
                     else{
-                        if(line.substr(last_pos, (i-last_pos)) == "you"){
-                            rack_ids.emplace(line.substr(last_pos, (i-last_pos)), 0);
+                        if(line.substr(last_pos, (i+1-last_pos)) == "you")
                             connections[current_id].push_back(0);
-                        }
-                        else if(line.substr(last_pos, (i-last_pos)) == "out"){
-                            rack_ids.emplace(line.substr(last_pos, (i-last_pos)), 1);
+                        else if(line.substr(last_pos, (i+1-last_pos)) == "out")
                             connections[current_id].push_back(1);
-                        }
                         else{
-                            rack_ids.emplace(line.substr(last_pos, (i-last_pos)), next_rack_id);
+                            rack_ids.emplace(line.substr(last_pos, (i+1-last_pos)), next_rack_id);
                             connections[current_id].push_back(next_rack_id++);
+                            connections.push_back(std::vector<unsigned int>());
                         }
                     }
                     break;
                 }
 
                 if(line[i]==' '){
-                    if(rack_ids.find(line.substr(last_pos, (i-last_pos-1))) != rack_ids.end()){
-                        connections[current_id].push_back(rack_ids[line.substr(last_pos, (i-last_pos-1))]);
+                    if(rack_ids.find(line.substr(last_pos, (i-last_pos))) != rack_ids.end()){
+                        connections[current_id].push_back(rack_ids[line.substr(last_pos, (i-last_pos))]);
                     }
                     else{
-                        if(line.substr(last_pos, (i-last_pos-1)) == "you"){
-                            rack_ids.emplace(line.substr(last_pos, (i-last_pos-1)), 0);
+                        if(line.substr(last_pos, (i-last_pos)) == "you")
                             connections[current_id].push_back(0);
-                        }
-                        else if(line.substr(last_pos, (i-last_pos-1)) == "out"){
-                            rack_ids.emplace(line.substr(last_pos, (i-last_pos-1)), 1);
+                        else if(line.substr(last_pos, (i-last_pos)) == "out")
                             connections[current_id].push_back(1);
-                        }
                         else{
-                            rack_ids.emplace(line.substr(last_pos, (i-last_pos-1)), next_rack_id);
+                            rack_ids.emplace(line.substr(last_pos, (i-last_pos)), next_rack_id);
                             connections[current_id].push_back(next_rack_id++);
+                            connections.push_back(std::vector<unsigned int>());
                         }
                     }
                     last_pos = i + 1;
                 }
             }
-            std::cout << "\n";
 	    }
         file.close();
     }
 }
 
 
+long long get_every_path(unsigned int current_rack, const std::vector<std::vector<unsigned int>> &connections){
+
+    if(current_rack == 1) return 1;
+    if(memory_paths[current_rack]!=-1) return memory_paths[current_rack];
+
+    long long num_paths{};
+    for(auto rack : connections[current_rack])
+        num_paths += get_every_path(rack, connections);
+    
+    memory_paths[current_rack] = num_paths;
+    return num_paths;
+}
+
+
 int main(){
     std::vector<std::vector<unsigned int>> connections;
-    get_data(connections, "test");
+    connections.push_back(std::vector<unsigned int>());
+    connections.push_back(std::vector<unsigned int>());
+    get_data(connections, "input");
 
-    for(auto i{0u};i<connections.size();++i){
-        std::cout << "In " << i << " there are:\n";
-        for(auto j{0u}; j<connections[i].size(); ++j){
-            std::cout << connections[i][j] << " ";
-        }
-        std::cout << "\n\n";
-    }
+    for(unsigned int i{}; i<connections.size(); ++i)
+        memory_paths.emplace(i,-1);
 
-    unsigned int result{};
+    long long result{ get_every_path(0, connections) };
     std::cout << "The final result of the operation is: " << result << "\n";
     return 0;
 }
--- 2025/Day-11/part2.cpp
+++ 2025/Day-11/part2.cpp
@@ -2,286 +2,127 @@
 #include <fstream>
 #include <vector>
 #include <string>
-#include <algorithm>
-#include <climits>
+#include <unordered_map>
+#include <tuple>
 
 
-long long gcd(long long num_a, long long num_b){
-    num_a = abs(num_a);
-    num_b = abs(num_b);
-    while(num_b){
-        long long temp{ num_b };
-        num_b = num_a%num_b;
-        num_a = temp;
-    }
-    return num_a;
-}
+// Memory for the search
+std::unordered_map<unsigned int, long long> memory_paths;
 
 
-void simplify(std::pair<long long, long long>& frac){
-    if(frac.first==0){
-        frac.second = 1;
-        return;
-    }
-    long long divisor{ gcd(frac.first, frac.second) };
-    frac.first /= divisor;
-    frac.second /= divisor;
-    if(frac.second<0){
-        frac.first = -frac.first;
-        frac.second = -frac.second;
-    }
-}
-
-
-std::pair<long long, long long> add(std::pair<long long, long long> frac_a, std::pair<long long, long long> frac_b){
-    std::pair<long long, long long> result ={frac_a.first*frac_b.second + frac_b.first*frac_a.second, frac_a.second*frac_b.second};
-    simplify(result);
-    return result;
-}
-
-
-std::pair<long long, long long> mul(std::pair<long long, long long> frac_a, std::pair<long long, long long> frac_b){
-    std::pair<long long, long long> result ={frac_a.first*frac_b.first, frac_a.second*frac_b.second};
-    simplify(result);
-    return result;
-}
-
-
-std::pair<long long, long long> divide(std::pair<long long, long long> frac_a, std::pair<long long, long long> frac_b){
-    if(frac_b.first==0) return std::make_pair(LLONG_MAX, 1LL);
-    std::pair<long long, long long> result ={frac_a.first*frac_b.second, frac_a.second*frac_b.first};
-    simplify(result);
-    return result;
-}
-
-
-struct SearchContext{
-    std::vector<std::vector<std::pair<long long, long long>>>& aug;
-    std::vector<int>& pivot_col;
-    std::vector<int>& free_vars;
-    int n, m, rank;
-    long long limit;
-    long long& best;
-};
-
-
-void search_recursive(int free_idx, std::vector<long long>& free_vals, SearchContext& ctx){
-    if(free_idx==(int)ctx.free_vars.size()){
-        std::vector<long long> solution(ctx.m, 0);
-        for(size_t i{}; i<ctx.free_vars.size(); ++i){
-            solution[ctx.free_vars[i]] = free_vals[i];
-        }
-        
-        bool is_valid{ true };
-        for(int row_idx{}; row_idx<ctx.rank; ++row_idx){
-            int col_idx{ ctx.pivot_col[row_idx] };
-            auto current_val{ ctx.aug[row_idx][ctx.m] };
-            for(int j{}; j<ctx.m; ++j){
-                if(j!=col_idx){
-                    auto temp_val{ mul(ctx.aug[row_idx][j],{solution[j], 1}) };
-                    current_val = add(current_val,{-temp_val.first, temp_val.second});
-                }
-            }
-            if(current_val.second!=1){
-                is_valid = false;
-                break;
-            }
-            if(current_val.first<0){
-                is_valid = false;
-                break;
-            }
-            
-            solution[col_idx] = current_val.first;
-        }
-        
-        if(is_valid){
-            long long total_presses{};
-            for(long long press_count : solution) total_presses += press_count;
-            ctx.best = std::min(ctx.best, total_presses);
-        }
-        return;
-    }
-    
-    for(long long val{}; val<=ctx.limit; ++val){
-        free_vals.push_back(val);
-        search_recursive(free_idx+1, free_vals, ctx);
-        free_vals.pop_back();
-        
-        if(ctx.best<LLONG_MAX){
-            long long partial_sum{};
-            for(long long v : free_vals) partial_sum += v;
-            if(partial_sum>=ctx.best) break;
-        }
-    }
-}
-
-
-long long get_min_pushes(const std::vector<long long>& goal, const std::vector<std::vector<int>>& buttons){
-    int num_counters{ static_cast<int>(goal.size()) };
-    int num_buttons{ static_cast<int>(buttons.size()) };
-    
-    if(num_buttons==0) return -1;
-    
-    std::vector<std::vector<std::pair<long long, long long>>> augmented_matrix(num_counters, std::vector<std::pair<long long, long long>>(num_buttons+1));
-    
-    for(int counter_idx{}; counter_idx<num_counters; ++counter_idx){
-        for(int button_idx{}; button_idx<num_buttons; ++button_idx){
-            augmented_matrix[counter_idx][button_idx] ={0, 1};
-        }
-        augmented_matrix[counter_idx][num_buttons] ={goal[counter_idx], 1};
-    }
-    
-    for(int btn_idx{}; btn_idx<num_buttons; ++btn_idx){
-        for(int light : buttons[btn_idx]){
-            augmented_matrix[light][btn_idx] ={1, 1};
-        }
-    }
-    
-    std::vector<int> pivot_col(num_counters, -1);
-    int rank{};
-    
-    for(int col{}; col<num_buttons && rank<num_counters; ++col){
-        int pivot_row{ -1 };
-        for(int row{rank}; row<num_counters; ++row){
-            if(augmented_matrix[row][col].first!=0){
-                pivot_row = row;
-                break;
-            }
-        }
-        
-        if(pivot_row==-1) continue;
-        
-        std::swap(augmented_matrix[rank], augmented_matrix[pivot_row]);
-        pivot_col[rank] = col;
-        
-        auto pivot_val{ augmented_matrix[rank][col] };
-        for(int c{}; c<=num_buttons; ++c){
-            augmented_matrix[rank][c] = divide(augmented_matrix[rank][c], pivot_val);
-        }
-        
-        for(int row{}; row<num_counters; ++row){
-            if(row==rank) continue;
-            if(augmented_matrix[row][col].first==0) continue;
-            
-            auto factor{ augmented_matrix[row][col] };
-            for(int c{}; c<=num_buttons; ++c){
-                auto temp{ mul(augmented_matrix[rank][c], factor) };
-                augmented_matrix[row][c] = add(augmented_matrix[row][c],{-temp.first, temp.second});
-            }
-        }
-        
-        ++rank;
-    }
-    
-    for(int row{rank}; row<num_counters; ++row){
-        if(augmented_matrix[row][num_buttons].first!=0){
-            return -1;
-        }
-    }
-    
-    std::vector<bool> is_basic(num_buttons, false);
-    for(int row{}; row<rank; ++row){
-        if(pivot_col[row]!=-1){
-            is_basic[pivot_col[row]] = true;
-        }
-    }
-    
-    std::vector<int> free_vars;
-    for(int col{}; col<num_buttons; ++col){
-        if(!is_basic[col]){
-            free_vars.push_back(col);
-        }
-    }
-    
-    if(free_vars.empty()){
-        std::vector<long long> solution(num_buttons, 0);
-        for(int row{}; row<rank; ++row){
-            int col{ pivot_col[row] };
-            auto x_val{ augmented_matrix[row][num_buttons] };
-            if(x_val.second!=1 || x_val.first<0){
-                return -1;
-            }
-            solution[col] = x_val.first;
-        }
-        long long total{};
-        for(long long v : solution) total += v;
-        return total;
-    }
-    
-    long long best_result{ LLONG_MAX };
-    
-    long long upper_limit{};
-    for(long long g : goal) upper_limit += g;
-    
-    SearchContext ctx ={augmented_matrix, pivot_col, free_vars, num_counters, num_buttons, rank, upper_limit, best_result};
-    std::vector<long long> free_vals;
-    search_recursive(0, free_vals, ctx);
-    
-    return (ctx.best==LLONG_MAX) ? -1 : ctx.best;
-}
-
-
-void get_data(std::vector<std::vector<std::vector<int>>>& buttons, std::vector<std::vector<long long>>& joltage_levels, std::string file_name){
-    std::string line;
+void get_data(std::vector<std::vector<unsigned int>> &connections, std::string file_name){
+    std::string line{};
     std::ifstream file(file_name);
 
+    unsigned long next_rack_id{ 4 };
+    std::unordered_map<std::string, unsigned int> rack_ids;
+    rack_ids.emplace("svr", 0);
+    rack_ids.emplace("out", 1);
+    rack_ids.emplace("fft", 2);
+    rack_ids.emplace("dac", 3);
+
     if(file.is_open()){
-        while(std::getline(file, line)){
-            int i{};
-            while(line[i]!=']') ++i;
-            ++i;
-            std::vector<std::vector<int>> button_list;
-            while(line[i]!='{'){
-                if(line[i]=='('){
-                    ++i;
-                    std::vector<int> affected;
-                    int num{};
-                    while(line[i]!=')'){
-                        if(isdigit(line[i])){
-                            num = num*10 + (line[i]-'0');
-                        }else if(line[i]==','){
-                            affected.push_back(num);
-                            num = 0;
-                        }
-                        ++i;
+    	while(getline(file,line)){
+            unsigned int current_id{};
+            unsigned int last_pos{};
+
+            for(unsigned int i{}; i<line.size(); ++i){
+                if(last_pos > i) continue;
+
+                if(line[i]==':'){
+                    if(rack_ids.find(line.substr(0,i)) != rack_ids.end())
+                        current_id = rack_ids[line.substr(0,i)];
+                    else{
+                        rack_ids.emplace(line.substr(0,i), next_rack_id);
+                        current_id = next_rack_id++;
+                        connections.push_back(std::vector<unsigned int>());
                     }
-                    affected.push_back(num);
-                    button_list.push_back(affected);
+                    last_pos = i + 2;
                 }
-                ++i;
-            }
-            buttons.push_back(button_list);
-            ++i;
-            std::vector<long long> joltage;
-            long long num{};
-            while(line[i]!='}'){
-                if(isdigit(line[i])){
-                    num = num*10 + (line[i]-'0');
-                }else if(line[i]==','){
-                    joltage.push_back(num);
-                    num = 0;
+
+                if(i==line.size()-1){
+                    if(rack_ids.find(line.substr(last_pos, (i+1-last_pos))) != rack_ids.end()){
+                        connections[current_id].push_back(rack_ids[line.substr(last_pos, (i+1-last_pos))]);
+                    }
+                    else{
+                        if(line.substr(last_pos, (i+1-last_pos)) == "svr")
+                            connections[current_id].push_back(0);
+                        else if(line.substr(last_pos, (i+1-last_pos)) == "out")
+                            connections[current_id].push_back(1);
+                        else if(line.substr(last_pos, (i+1-last_pos)) == "fft")
+                            connections[current_id].push_back(2);
+                        else if(line.substr(last_pos, (i+1-last_pos)) == "dac")
+                            connections[current_id].push_back(3);
+                        else{
+                            rack_ids.emplace(line.substr(last_pos, (i+1-last_pos)), next_rack_id);
+                            connections[current_id].push_back(next_rack_id++);
+                            connections.push_back(std::vector<unsigned int>());
+                        }
+                    }
+                    break;
                 }
-                ++i;
+
+                if(line[i]==' '){
+                    if(rack_ids.find(line.substr(last_pos, (i-last_pos))) != rack_ids.end()){
+                        connections[current_id].push_back(rack_ids[line.substr(last_pos, (i-last_pos))]);
+                    }
+                    else{
+                        if(line.substr(last_pos, (i-last_pos)) == "svr")
+                            connections[current_id].push_back(0);
+                        else if(line.substr(last_pos, (i-last_pos)) == "out")
+                            connections[current_id].push_back(1);
+                        else if(line.substr(last_pos, (i-last_pos)) == "fft")
+                            connections[current_id].push_back(2);
+                        else if(line.substr(last_pos, (i-last_pos)) == "dac")
+                            connections[current_id].push_back(3);
+                        else{
+                            rack_ids.emplace(line.substr(last_pos, (i-last_pos)), next_rack_id);
+                            connections[current_id].push_back(next_rack_id++);
+                            connections.push_back(std::vector<unsigned int>());
+                        }
+                    }
+                    last_pos = i + 1;
+                }
             }
-            joltage.push_back(num);
-            joltage_levels.push_back(joltage);
-        }
+	    }
         file.close();
     }
 }
 
 
-int main(){
-    std::vector<std::vector<std::vector<int>>> buttons;
-    std::vector<std::vector<long long>> joltage_levels;
-    get_data(buttons, joltage_levels, "input");
+// New logic for part 2
+long long get_every_path(unsigned int current_rack, bool fft, bool dac, const std::vector<std::vector<unsigned int>> &connections){
 
-    long long result{}, op_result{};
-    for(size_t i{}; i<joltage_levels.size(); ++i){
-        result += op_result = get_min_pushes(joltage_levels[i], buttons[i]);
-        std::cout << "Completed line " << i+1 << " out of " << joltage_levels.size() << " with result: " << op_result << "\n";
+    if(current_rack==1){
+        if(fft && dac) return 1;
+        return 0;
     }
+    //if(memory_paths[current_rack]!=-1) return memory_paths[current_rack];
 
-    std::cout << "The final result is: " << result << "\n";
+    if(current_rack==2) fft = true;
+    if(current_rack==3) dac = true;
+
+    long long num_paths{};
+    for(auto rack : connections[current_rack])
+        num_paths += get_every_path(rack, fft, dac, connections);
+    
+    //memory_paths[current_rack] = num_paths;
+    return num_paths;
+}
+
+
+int main(){
+    std::vector<std::vector<unsigned int>> connections;
+    connections.push_back(std::vector<unsigned int>());
+    connections.push_back(std::vector<unsigned int>());
+    connections.push_back(std::vector<unsigned int>());
+    connections.push_back(std::vector<unsigned int>());
+    get_data(connections, "test");
+
+    for(unsigned int i{}; i<connections.size(); ++i)
+        memory_paths.emplace(i,-1);
+
+    long long result{ get_every_path(0, false, false, connections) };
+    std::cout << "The final result of the operation is: " << result << "\n";
     return 0;
 }
